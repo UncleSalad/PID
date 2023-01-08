96bb87d1fbd99061f70cbd28c7719c21f295c365
サラダ <48404900+UncleSalad@users.noreply.github.com>
2023-01-08 18:04:28 +0300

    Update PID.h

--- PID.h
+++ PID.h
@@ -1,54 +1,47 @@
-#ifndef PID_H
-#define PID_H
+#pragma once
 
 #include "Arduino.h"
 
 class PID
 {
-    // Private
     private:
-    float _kP, _kI, _kD, _setpoint;
-    float _integral, _diff;
+    // Minimum and maximum output range
     float _min, _max;
-    uint32_t _dt;
 
-    // Public
+    // Sample rate in seconds
+    float _dt_s;
+
+    // Integral component
+    float _integral;
+
+    // Derivative component
+    float _derivative;
+
+    // Regulation error
+    float _error, _prevError;
+
     public:
-    PID(float kP, float kI, float kD, uint32_t dt, float min, float max);
+    // Constructor
+    PID(float Kp, float Ki, float Kd, float dT = 100);
 
-    void setP(float kP) { _kP = kP; }
-    void setI(float kI) { _kI = kI; }
-    void setD(float kD) { _kI = kD; }
-    void setDT(float dt) { _dt = dt; }
-    void setPoint(float setpoint) { _setpoint = setpoint; }
+    // PID coefficients
+    float kP, kI, kD;
 
-    float getP() { return _kP; }
-    float getI() { return _kI; }
-    float getD() { return _kD; }
-    uint32_t getDT() { return _dt; }
-    float getPoint() { return _setpoint; }
+    // Sample rate
+    float dt;
 
-    float getResult(float input);
+    // Input
+    float input;
+
+    // Output
+    float output;
+
+    // Set point
+    float setpoint;
+
+    // Calculate
+    float getResult();
+
+    // Constrain output value
+    void setLimits(float min, float max);
 };
-
-// Как юзать
-/*
-    1. Создай объект
-    PID твоё_имя
-    (или)
-    PID твоё_имя(kP, kI, kD, dt, minOut, maxOut);
-
-    2. Задай, к чему привести систему
-    твоё_имя.setPoint(значение);
-
-    3. Заставь считать через каждый период dt
-    static uint32_t tmr = 0;
-    for (millis() - tmr > dt)
-    {
-        out = твоё_имя.getResult(значение_с_датчика_температуры_допустим);
-    }
-
-    4. Расскажи своей мамочке, какой ты молодец-холодец))
-*/
-
-#endif
